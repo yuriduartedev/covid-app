95c9d1a0a65830410c4b8bb1b11c5396dd569567
Yuri Duarte <yuriduartedev@gmail.com>
2020-06-25 04:26:37 -0300

    Adiciona Link na logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Header = () => {
-  return (    
+  return (
     <nav>
-      <h1>CORONAVÍRUS</h1>
+      <Link to="/">
+        <h1>CORONAVÍRUS</h1>
+      </Link>
       <div className="links">
         <Link className="first after" to="/">Painel Geral</Link>
         <Link className="first after" to="/sobre">Sobre</Link>
